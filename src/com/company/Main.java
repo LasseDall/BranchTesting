754eb374b8f30c5a31d35cc4b16fffda9340e088
LasseDall <lass794rrstud.kea.dk>
2022-04-05 09:18:07 +0200

    main opdated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        cat.makeSound();
+        dog.makeSound();
     }
 }
